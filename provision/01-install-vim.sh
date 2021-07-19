72c40f3bb264b6381b6668bdbd6bfb7da393a79d
Reiner Rottmann <reiner@rottmann.it>
2021-07-19 22:22:36 +0200

    Example provisioning script is more robust.

--- provision/01-install-vim.sh
+++ provision/01-install-vim.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-apt-get update && apt-get install -y vim
+# This is an example provisioning script that runs after system boot.
+# A network link and IP via DHCP is required.
+
+# We wait for establishing a link for 300 seconds.
+timeout=$(date -ud "300 seconds" +%s)
+while true; do
+   [[ $(date -u +%s) -le $timeout ]] || break
+   nmcli general status 2>/dev/null | grep -q ^connected && break
+   sleep 15
+done
+
+# Check whether we have internet access and install vim-tiny package.
+ping -q -c 1 google.com 2>&1 >/dev/null && apt-get update && apt-get install -y vim-tiny
